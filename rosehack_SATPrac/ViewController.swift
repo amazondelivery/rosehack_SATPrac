d62b7768e0dc93c0e00af8b11a160856d373dbae
amazondelivery <mr.vikram.balaji@gmail.com>
2022-01-15 15:11:52 -0800

    mainthings

---
+++ rosehack_SATPrac/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  rosehack_SATPrac
+//
+//  Created by Vikram B on 1/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let mainColor = UIColor(hue: 0.0806, saturation: 0.2, brightness: 0.88, alpha: 1.0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = mainColor
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ rosehack_SATPrac/passageStruct.swift
@@ -0,0 +1,8 @@
+//
+//  passageStruct.swift
+//  rosehack_SATPrac
+//
+//  Created by Vikram B on 1/15/22.
+//
+
+import Foundation
